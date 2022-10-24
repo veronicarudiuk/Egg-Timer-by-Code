1f60d194f91b9517a4c31d0273fa16e811eabf22
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-10-24 18:38:47 +0400

    add appearance

--- Egg Timer by Code/ViewController.swift
+++ Egg Timer by Code/ViewController.swift
@@ -9,15 +9,85 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    //    let softImage = UIImageView(image: UIImage(named: "Soft egg"))
+    let softButton = UIButton()
+    let mediumButton = UIButton()
+    let hardButton = UIButton()
+    let softImage = UIImageView(image: UIImage(named: "Soft"))
+    let mediumImage = UIImageView(image: UIImage(named: "Medium"))
+    let hardImage = UIImageView(image: UIImage(named: "Hard"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.7949336767, green: 0.9477056861, blue: 0.9900913835, alpha: 1)
+        
+        let title = UILabel()
+        title.text = "How do you like your eggs?"
+        title.font = .systemFont(ofSize: 30)
+        title.textColor = .darkGray
+        title.textAlignment = .center
+        
+        let softView = UIView(button: softButton, image: softImage, title: "Soft")
+        let mediumView = UIView(button: mediumButton, image: mediumImage, title: "Medium")
+        let hardView = UIView(button: hardButton, image: hardImage, title: "Hard")
+
+        
+        
+        let eggStackView = UIStackView(arrangedSubviews: [softView, mediumView, hardView])
+        eggStackView.axis = .horizontal
+        eggStackView.distribution = .fillEqually
+        eggStackView.spacing = 20
+        
+        let progressBar = UIProgressView(progressViewStyle: .bar)
+        progressBar.progressTintColor = .systemYellow
+        progressBar.trackTintColor = .systemGray
+        let progressView = UIView()
+        progressView.addSubview(progressBar)
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            progressBar.centerYAnchor.constraint(equalTo: progressView.centerYAnchor),
+            progressBar.leadingAnchor.constraint(equalTo: progressView.leadingAnchor),
+            progressBar.trailingAnchor.constraint(equalTo: progressView.trailingAnchor),
+        ])
+        
+        let mainStackView = UIStackView(arrangedSubviews: [title, eggStackView, progressView])
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fillEqually
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
     }
-
-
 }
-
+extension UIView {
+    convenience init(button: UIButton, image: UIImageView, title: String) {
+        self.init()
+        self.addSubview(image)
+        self.addSubview(button)
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            image.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            image.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            image.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .black)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+    }
+}
 
 
 struct SwiftUIController: UIViewControllerRepresentable {
@@ -34,6 +104,6 @@ struct SwiftUIController: UIViewControllerRepresentable {
 
 struct SwiftUIController_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIController()
+        SwiftUIController().edgesIgnoringSafeArea(.all)
     }
 }
